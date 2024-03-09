0d92a7f5715f9bf60f2090c05cc8bfb78d07d0e2
fzl <fzeli@163.com>
2024-03-09 10:02:16 +0800

    [refactor] Replace inheritance with composition to control the interface

--- DwarfTag.hpp
+++ DwarfTag.hpp
@@ -12,19 +12,20 @@ inline uint64_t GetDW_AT_byte_size(llvm::DWARFDie die) {
 inline const char *GetDW_AT_name(llvm::DWARFDie die) {
   return die.find(llvm::dwarf::DW_AT_name)->getAsCString().get();
 }
-struct DwarfTagArrayType : llvm::DWARFDie { // DW_TAG_array_type
-  DwarfTagArrayType(DWARFDie die) : DWARFDie{die} {
+struct DwarfTagArrayType { // DW_TAG_array_type
+  llvm::DWARFDie die;
+  DwarfTagArrayType(llvm::DWARFDie die) : die{die} {
     assert(die.getTag() == llvm::dwarf::DW_TAG_array_type);
   }
   // DWARF5标准说array_type一定有DW_AT_name，但dump出来好像没有
-  DWARFDie ElementType() const { // array一定有type
-    auto type = find(llvm::dwarf::DW_AT_type);
-    auto unit = getDwarfUnit();
+  llvm::DWARFDie ElementType() const { // array一定有type
+    auto type = die.find(llvm::dwarf::DW_AT_type);
+    auto unit = die.getDwarfUnit();
     auto offset = type->getRawUValue() + unit->getOffset();
     return unit->getDIEForOffset(offset);
   }
   uint64_t Length() const {
-    for (auto child = getFirstChild(); child; child = child.getSibling()) {
+    for (auto child = die.getFirstChild(); child; child = child.getSibling()) {
       switch (child.getTag()) { // 长度在subrange或enumeration
       case llvm::dwarf::DW_TAG_subrange_type: { // gcc用的是这个
         auto value = child.find(llvm::dwarf::DW_AT_upper_bound).value();
@@ -40,31 +41,33 @@ struct DwarfTagArrayType : llvm::DWARFDie { // DW_TAG_array_type
     assert(0);
   }
 };
-struct DwarfTagMember : llvm::DWARFDie { // DW_TAG_member
-  DwarfTagMember(DWARFDie die) : DWARFDie{die} {
+struct DwarfTagMember { // DW_TAG_member
+  llvm::DWARFDie die;
+  DwarfTagMember(llvm::DWARFDie die) : die{die} {
     assert(die.getTag() == llvm::dwarf::DW_TAG_member);
   }
   const char *Name() const { // member一定有name，除非是匿名union
-    return GetDW_AT_name(*this);
+    return GetDW_AT_name(die);
   }
-  DWARFDie Type() const { // member一定有type
-    auto offset = this->find(llvm::dwarf::DW_AT_type)->getRawUValue();
-    auto unit = getDwarfUnit();
+  llvm::DWARFDie Type() const { // member一定有type
+    auto offset = die.find(llvm::dwarf::DW_AT_type)->getRawUValue();
+    auto unit = die.getDwarfUnit();
     return unit->getDIEForOffset(offset + unit->getOffset());
   }
   uint64_t MemberOffset() const { // output of offsetof()
                                   // TODO 可能没有data_member_location
-    return find(llvm::dwarf::DW_AT_data_member_location)
+    return die.find(llvm::dwarf::DW_AT_data_member_location)
         ->getAsUnsignedConstant()
         .value();
   }
 };
-struct DwarfTagStructureType : llvm::DWARFDie {
+struct DwarfTagStructureType {
+  llvm::DWARFDie die;
   // use `for(auto child = getChild(); child; child = child.getSibling())` to
   // iterate members
   // TODO tag name
-  DwarfTagStructureType(DWARFDie die) : DWARFDie{die} {
+  DwarfTagStructureType(llvm::DWARFDie die) : die{die} {
     assert(die.getTag() == llvm::dwarf::DW_TAG_structure_type);
   }
-  uint64_t ByteSize() const { return GetDW_AT_byte_size(*this); }
+  uint64_t ByteSize() const { return GetDW_AT_byte_size(die); }
 };
